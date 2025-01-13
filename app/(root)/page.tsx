488ad4be9c081bbb0fd3686865dc79178f154cd3
anhthtservice@gmail.com <anhthtservice@gmail.com>
2025-01-13 14:09:53 +0700

    feat: complete navigation page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 import HeaderBox from "@/components/header-box";
+import RightSidebar from "@/components/right-sidebar";
 import TotalBalanceBox from "@/components/total-balance";
 import React from "react";
 
 const HomePage = () => {
+  const isLogged = { firstName: "John", lastName: "Doe" };
   return (
     <section className="home">
       <div className="home-content">
@@ -13,9 +15,16 @@ const HomePage = () => {
             user="John Doe"
             subtext="Access and manage your account and transactions efficiently."
           />
-          <TotalBalanceBox />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1260.34}
+          />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar user={isLogged} transactions={[]} banks={[]} />
     </section>
   );
 };
